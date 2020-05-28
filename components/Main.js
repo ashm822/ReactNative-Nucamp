6b9aed90160e32ca3f8ca752aec4808e9c08602f
Ashraf Madina <ashraf.madina@asset-map.com>
2020-05-27 20:54:22 -0400

    second component

--- components/Main.js
+++ components/Main.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import Directory from './Directory';
 import { CAMPSITES } from '../shared/campsites';
+import CampsiteInfo from './CampsiteInfo';
+import { View } from 'react-native';
+
+
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          campsites: CAMPSITES
+          campsites: CAMPSITES,
+          selectedCampsite: null
         };
     }
 
-    render() {
-        return <Directory campsites={this.state.campsites} />;
+    onCampsiteSelect(campsiteId) {
+        this.setState({selectedCampsite: campsiteId});
     }
+
+    render() {
+        return (
+                <View style={{flex: 1}}>
+                    <Directory campsites={this.state.campsites} 
+                        onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
+                    />
+
+                    <CampsiteInfo campsite={this.state.campsites.filter(campsite => 
+                        campsite.id === this.state.selectedCampsite)[0]}                        
+                    />
+                    
+                </View>
+    )}
 }
 
 export default Main;
