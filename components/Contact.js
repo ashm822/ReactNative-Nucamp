6c16ba3b4d723601ce08c4f9d86c41077bfb9b1c
Ashraf Madina <ashraf.madina@asset-map.com>
2020-06-18 21:39:22 -0400

    share api

--- components/Contact.js
+++ components/Contact.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
-import { Card, Text } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
+
 
 
 class Contact extends Component {
 
     static navigationOptions = {
         title: 'Contact Us' 
+    }
+
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        })
     }
 
     render() {
@@ -25,6 +35,17 @@ class Contact extends Component {
                         Phone: 1-206-555-1234 {"\n"}
                         Email: Campsites@nucamp.com
                     </Text>
+                    <Button
+                            title="Send Email"
+                            buttonStyle={{backgroundColor: '#5637DD', margin: 40}}
+                            icon={<Icon
+                                name='envelope-o'
+                                type='font-awesome'
+                                color='#fff'
+                                iconStyle={{marginRight: 10}}
+                            />}
+                            onPress={() => this.sendMail()}
+                        />
                     </Card>
                 </Animatable.View>
             </ScrollView>
